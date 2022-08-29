305e5396a8c559355c70452ce85b7be46690e535
Juan O_o <63573126+juan-Br@users.noreply.github.com>
2022-08-29 15:42:25 -0300

    fist

---
+++ script.js
@@ -0,0 +1,8 @@
+function buttonhat(){
+	var hatinfo= document.getElementById("hatinfo");
+	if (hatinfo.style.display === "none"){
+		hatinfo.style.display = "inline-block";
+	} else{
+		hatinfo.style.display="none";
+	}
+}
